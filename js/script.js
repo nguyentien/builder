e10093b5e14f64dabc67780dabe149d1d4241418
tiennguyen <tiennguyen@success-ss.com.vn>
2012-11-12 18:39:48 +0700

    - remove min

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$('#preview').coffeeBuilder {
+	manifest: {
+		panels: {
+			page: {
+				name: 'Page',
+				customizations: {
+					background: {
+						name: 'Backgro'
+						selector: 'body',
+						
+					}
+				}
+			}
+		}
+	}
+};
